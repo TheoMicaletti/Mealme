5acdebd1662a83cfe621efbf925d8f58e5897fb1
mclemensat <97509715+mclemensat@users.noreply.github.com>
2022-04-13 14:45:49 +0200

    Letseat button (#8)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,21 @@ import Footer from "@components/Footer";
 export default function Home() {
   return (
     <>
-      <h1 className="mt-8 mb-4 mt-24 lg:text-6xl text-4xl dark:text-white text-center text-mada">
+      <h1 className="mt-24 lg:text-6xl text-4xl dark:text-white text-center text-mada">
         Let&apos;s{" "}
         <span className="text-gradient text-atma font-bold">cook!</span>
       </h1>
       <Header />
       <Autocomplete data={autoCompleteData} />
+      <div className="mt-24 flex justify-center">
+        <button
+          type="button"
+          className="w-32 lg:text-2xl text-2xl border-2 rounded-3xl dark:text-white text-mada"
+        >
+          Let&apos;s{" "}
+          <span className="text-gradient text-atma font-bold">eat!</span>
+        </button>
+      </div>
       <Footer />
     </>
   );
