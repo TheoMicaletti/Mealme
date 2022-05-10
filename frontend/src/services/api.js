92521a4433db73783492877c2a304fef9260378b
Vincent Vaur <vaur.vincent@gmail.com>
2022-05-10 19:13:57 +0200

    fix deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -27,7 +27,7 @@ export const getRecipeById = async (id) => {
 };
 
 // Appel ingredients database
-const DB_URL = import.meta.env.BACKEND_URL || "http://localhost:5000";
+const DB_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 export const getIngredientsList = async () => {
   return (await axios.get(`${DB_URL}/ingredients`)).data;
