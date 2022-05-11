7c9df808f148e8f0f5159c570884fc7764ec7f17
Vincent Vaur <vaur.vincent@gmail.com>
2022-05-11 09:58:18 +0200

    Fix routing for REACT app

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,8 +22,10 @@ app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
 app.use(router);
 
 // Redirect all requests to the REACT app
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "frontend", "dist"));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "frontend", "dist", "index.html")
+  );
 });
 
 // ready to export
